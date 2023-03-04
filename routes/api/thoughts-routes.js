a3c1d00dab9011397a6456cbd5236d33fef8597c
benkabundi01 <benkabundi01@gmail.com>
2023-03-04 06:39:16 -0500

    finished routes

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -15,18 +15,18 @@ const {
  * getting all thoughts/creating a new thought
  * url here is localhost:3001/api/thoughts
  */
-router.route('/').get().post();
+router.route('/').get(gettingAllThoughts).post(createNewThought);
 
 /**
  * getting/updating/deleting by ID
  * url here is localhost:3001/api/thoughts:id
  */
-router.route('/:id').get().put().delete();
+router.route('/:id').get(getOneThoughtByID).put(updatingThought).delete(deletingThought);
 
 /**
  * create a reaction/ delete a reaction
  * url here is localhost:3001/api/thoughts/:thoughtId/reactions
  */
-router.route('/:thoughtId/reactions').post().delete();
+router.route('/:thoughtId/reactions').post(addingReaction).delete(deletingReaction);
 
 module.exports = router;
--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -2,24 +2,30 @@
 const router = require('express').Router();
 //require all of the methods for CRUD from the users controller
 const {
-
+    gettingAllUsers,
+    getOneUserByID,
+    createNewUser,
+    updatingUser,
+    deletingUser,
+    addingFriend,
+    deletingFriend
 } = require('../../controllers/user-Controller');
 
 /**
  *  getting all users, or creating a new user
  * url here is localhost:3001/api/users
  */
-router.route('/').get().post;
+router.route('/').get(gettingAllUsers).post(createNewUser);
 
 /**
  * getting, updating, and deleting a user by _id
  */
-router.route('/:id').get().put().delete();
+router.route('/:id').get(getOneUserByID).put(updatingUser).delete(deletingUser);
 
 /**
  * adding a new friend to a user's friend list, or removing them from friend list
  * url here is localhost:3001/api/users/:userId/friends/:friendID
  */
-router.route('/:userId/friends/:friendID').post().delete();
+router.route('/:userId/friends/:friendID').post(addingFriend).delete(deletingFriend);
 
 module.exports = router;
