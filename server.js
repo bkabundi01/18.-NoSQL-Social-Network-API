4c802328921f089495e141a35f9bc0dea678a713
benkabundi01 <benkabundi01@gmail.com>
2023-03-04 03:47:20 -0500

    Finished server.js file and added connection.js file

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const routes = require('./routes');
 //set up server and the port number
 const app = express();
 const port = process.env.PORT || 3001;
+
+//allowing the use of the data from response
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//calling the routes; url here is localhost:3001
+app.use(routes);
+
+//connecting to mongoDB and starting the server
+db.once('open', () => {
+    app.listen(port, () => {
+        console.log(`Server starting on port ${port}....Now listening....`);
+    });
+});
