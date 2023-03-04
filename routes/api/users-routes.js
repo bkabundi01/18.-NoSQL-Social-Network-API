3ba03b5b032c7b31af44819967e0074dbdd37f73
benkabundi01 <benkabundi01@gmail.com>
2023-03-04 04:57:21 -0500

    Finished the users routes file

---
+++ routes/api/users-routes.js
@@ -0,0 +1,25 @@
+//require Router() from express
+const router = require('express').Router();
+//require all of the methods from the users controller
+const {
+
+} = require('../../controllers/userController');
+
+/**
+ *  getting all users, or creating a new user
+ * url here is localhost:3001/api/users
+ */
+router.route('/').get().post;
+
+/**
+ * getting, updating, and deleting a user by _id
+ */
+router.route('/:id').get().put().delete();
+
+/**
+ * adding a new friend to a user's friend list, or removing them from friend list
+ * url here is localhost:3001/api/users/:userId/friends/:friendID
+ */
+router.route('/:userId/friends/:friendID').post().delete();
+
+module.exports = router;
